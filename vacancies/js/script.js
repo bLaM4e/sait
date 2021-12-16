1fd682b28f84497c9d9dd9ea59debc0408009df5
Dmitry <konnerspz@mail.ru>
2021-12-16 07:13:55 +0300

    error correction

--- vacancies/js/script.js
+++ vacancies/js/script.js
@@ -3,9 +3,9 @@
 const dropDownList = document.querySelectorAll('.form_select');
 
 for (const el of dropDownList) {
-	el.addEventListener('click', event => {
-	event.target.nextElementSibling.classList.toggle('form_active');
-	event.target.childNodes[3].classList.toggle('form_arrov_active');
+	const arrow = el.querySelectorAll('.form_arrow')
+	el.addEventListener('click', () => {
+		el.nextElementSibling.classList.toggle('form_active');
+		el.childNodes[3].classList.toggle('form_arrov_active');
 	});
 }
-
