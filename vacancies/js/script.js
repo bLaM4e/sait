4494b4fd0787d096703653b4f3276cd17294dc1b
Dmitry <konnerspz@mail.ru>
2021-12-17 16:28:15 +0300

    add resume js

--- vacancies/js/script.js
+++ vacancies/js/script.js
@@ -7,36 +7,60 @@ const selectMenu = {
 		for (let i = 0; i < this.selectItems.length; i++) {
 			const selectBlock = this.selectItems[i];
 			let dropOut = this.selectItems[i].nextElementSibling;
-			let activeClass = '';
 
 			selectBlock.addEventListener('click', () => {
-				activeClass = this.classSelection(i);
 
 				if (![...dropOut.classList].includes('form_active')) {
-					this.addActiveClass(dropOut, selectBlock, activeClass);
+					this.addActiveClass(dropOut, selectBlock);
+
+					dropOut.childNodes[1].addEventListener('click', this.userSelectionFunc);
 				} else {
-					this.removeActiveClass(dropOut, selectBlock, activeClass);
+					this.removeActiveClass(dropOut, selectBlock);
 				}
 			});
 		}
 	},
 
-	classSelection(idx) {
-		if (idx === 0) return 'out_ul_active1';
-		
-		return 'out_ul_active2';
+	userSelectionFunc(event) {
+		let parentBlock = '';
+		let userSelection = '';
+		let dropOut = '';
+
+		if (event.target.tagName === 'LI') {
+			parentBlock = event.target.parentElement.parentElement.parentElement.querySelector('.text_in_block');
+			dropOut = event.target.parentElement.parentElement
+			userSelection = event.target.firstChild.textContent;
+		} else {
+			parentBlock = event.target.parentElement.parentElement.parentElement.parentElement.querySelector('.text_in_block');
+			dropOut = event.target.parentElement.parentElement.parentElement
+			userSelection = event.target.textContent;
+		}
+		parentBlock.textContent = userSelection;
+
+		selectMenu.removeActiveClass(dropOut, dropOut.parentElement.querySelector('.form_select'));
 	},
 
-	addActiveClass(dropOut, selectBlock, activeClass) {
+	addActiveClass(dropOut, selectBlock) {
 		dropOut.classList.add('form_active');
 		selectBlock.childNodes[3].classList.add('form_arrow_active');
 
-		setTimeout(() => 
-			dropOut.childNodes[1].classList.add(activeClass), 0);
+		setTimeout(() => {
+			const dropOutBlock = dropOut.childNodes[1];
+			const dropOutBlockItems = dropOutBlock.childNodes;
+
+			let sumHeights = 0;
+			for (const el of dropOutBlockItems) {
+				if (el.tagName === 'LI') {
+					sumHeights += el.clientHeight + 1;
+				}
+			}
+
+			dropOutBlock.style.height = sumHeights + "px";
+		}, 0)
 	},
 
-	removeActiveClass(dropOut, selectBlock, activeClass) {
-		dropOut.childNodes[1].classList.remove(activeClass);
+	removeActiveClass(dropOut, selectBlock) {
+		dropOut.childNodes[1].style.height = 0;
 		selectBlock.childNodes[3].classList.remove('form_arrow_active');
 
 		setTimeout(() => {
@@ -64,5 +88,23 @@ const scroll = {
 	}
 }
 
+const downloadResume = {
+	inputButton: document.querySelector('.input_file'),
+	resumeEmpty: document.querySelector('.form_resume_empty'),
+	resumeOk: document.querySelector('.form_resume_ok'),
+	resumeTitle: document.querySelector('.user_name_resume'),
+
+	upload() {
+		this.inputButton.addEventListener('change', () => {
+			const nameVal = document.querySelector('.form_user_name').value;
+
+			this.resumeEmpty.classList.add('resume_empty_toggle');
+			this.resumeOk.classList.add('resume_ok_toggle');
+			this.resumeTitle.textContent = nameVal + ' - резюме';
+		});
+	}
+}
+
 selectMenu.init();
 scroll.toElement();
+downloadResume.upload();
